0f5c5b490e391cbb119caa22f63f0141a01b91e1
samy <samy@samyvs.com>
2025-07-08 16:46:27 -0700

    Add organize imports plugin

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   site: 'https://www.bluealpha.ai',
--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'eslint/config'
 import eslintConfigPrettier from 'eslint-config-prettier/flat'
 import eslintPluginAstro from 'eslint-plugin-astro'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
   ...eslintPluginAstro.configs.recommended,
