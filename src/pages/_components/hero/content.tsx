1f07269b9c992c45581488cdb24cfb82e2b664c7
samy <samy@samyvs.com>
2025-07-10 10:27:23 -0700

    Add content component

---
+++ src/pages/_components/hero/content.tsx
@@ -0,0 +1,21 @@
+import { smoothTransition } from '#utils/transitions.ts'
+import { motion } from 'motion/react'
+import { HeroActions } from './actions'
+import { Description } from './description'
+import { Title } from './title'
+
+export function Content() {
+  return (
+    <motion.div
+      className='flex flex-col gap-8'
+      initial={{ opacity: 0, y: 40 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={smoothTransition}>
+      <div className='flex flex-col gap-6'>
+        <Title />
+        <Description />
+      </div>
+      <HeroActions />
+    </motion.div>
+  )
+}
