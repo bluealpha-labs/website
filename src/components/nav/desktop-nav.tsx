9419ab524fa488909c327172351d88d5c84d1a25
samy <samy@samyvs.com>
2025-07-10 11:37:31 -0700

    Add desktop nav

---
+++ src/components/nav/desktop-nav.tsx
@@ -0,0 +1,37 @@
+import { Button } from '#components/button.tsx'
+import { Link } from '#components/link.tsx'
+import { $showCta } from '#stores/show-cta.tsx'
+import { snappyTransition } from '#utils/transitions.ts'
+import { useStore } from '@nanostores/react'
+import { AnimatePresence } from 'motion/react'
+
+export function DesktopNav() {
+  const showCta = useStore($showCta)
+
+  return (
+    <div className='sticky top-0 z-10'>
+      <nav className='absolute top-6 left-1/2 h-15 w-146 -translate-x-1/2 items-center gap-6 rounded-full bg-stone-200/50 px-6 py-2 backdrop-blur-xl max-md:hidden md:flex'>
+        <div className='flex-1'>
+          <a
+            href='/'
+            className='text-xl font-semibold tracking-[-.01em]'>
+            BlueAlpha
+          </a>
+        </div>
+        <Link href='/about'>About</Link>
+        <Link href='/case-studies'>Case studies</Link>
+        <AnimatePresence mode='popLayout'>
+          {showCta && (
+            <Button
+              className='-me-3'
+              initial={{ y: 20, opacity: 0, transition: snappyTransition }}
+              animate={{ y: 0, opacity: 1, transition: snappyTransition }}
+              exit={{ y: 20, opacity: 0, transition: snappyTransition }}>
+              Call with founder
+            </Button>
+          )}
+        </AnimatePresence>
+      </nav>
+    </div>
+  )
+}
