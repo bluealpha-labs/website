956e133e15b4c8190a225e011cfdaa9862f23845
samy <samy@samyvs.com>
2025-07-09 23:55:20 -0700

    Update button component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,11 +4,11 @@ import type { ComponentProps } from 'react'
 import { tv, type VariantProps } from 'tailwind-variants'
 
 export const buttonStyles = tv({
-  base: 'rounded-full flex items-center gap-2 h-11 font-semibold tracking-wide px-4 cursor-pointer transition-colors ease-out',
+  base: 'flex h-11 cursor-pointer items-center gap-2 rounded-full px-4 font-semibold tracking-[.01em] transition-colors ease-out',
   variants: {
     variant: {
       default: 'bg-stone-900 text-white hover:bg-stone-800',
-      outline: 'border border-stone-200 hover:bg-stone-50 hover:border-stone-300'
+      outline: 'border border-stone-200 hover:border-stone-300 hover:bg-stone-50'
     }
   },
   defaultVariants: {
