c6920465d2d013ed8a19e53ecd62ffed90ec355c
samy <samy@samyvs.com>
2025-07-10 10:18:41 -0700

    Add description component

---
+++ src/pages/_components/hero/description.tsx
@@ -0,0 +1,7 @@
+export function Description() {
+  return (
+    <p className='max-w-lg text-lg font-[450] text-stone-500 md:max-w-lg lg:max-w-xl lg:text-xl'>
+      Put AI and data science in the hands of your marketing team with an end-to-end automation platform.
+    </p>
+  )
+}
