3dd5f55532afadff9e2e583c450ddb5f22914caa
samy <samy@samyvs.com>
2025-07-10 10:17:54 -0700

    Update title component

---
+++ src/pages/_components/hero/title.tsx
@@ -0,0 +1,7 @@
+export function Title() {
+  return (
+    <h1 className='max-w-lg text-5xl font-[650] tracking-[-.01em] md:max-w-2xl md:text-6xl lg:max-w-3xl lg:text-7xl'>
+      Make every marketing dollar work harder
+    </h1>
+  )
+}
