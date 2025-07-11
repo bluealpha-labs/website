41b90cfa3f3e8b354c28ca0b04d1466bb14bbc01
samy <samy@samyvs.com>
2025-07-10 18:37:01 -0700

    Update desktop nav component

--- src/components/nav/desktop-nav.tsx
+++ src/components/nav/desktop-nav.tsx
@@ -1,5 +1,6 @@
 import { Button } from '#components/button.tsx'
 import { Link } from '#components/link.tsx'
+import { Logo } from '#components/logo.tsx'
 import { $showCta } from '#stores/show-cta.tsx'
 import { snappyTransition } from '#utils/transitions.ts'
 import { useStore } from '@nanostores/react'
@@ -9,13 +10,14 @@ export function DesktopNav() {
   const showCta = useStore($showCta)
 
   return (
-    <div className='sticky top-0 z-10'>
+    <div className='sticky top-0 z-50'>
       <nav className='absolute top-6 left-1/2 h-15 w-146 -translate-x-1/2 items-center gap-6 rounded-full bg-stone-200/50 px-6 py-2 backdrop-blur-xl max-md:hidden md:flex'>
         <div className='flex-1'>
           <a
             href='/'
-            className='text-xl font-semibold tracking-[-.01em]'>
-            BlueAlpha
+            className='flex items-center gap-2'>
+            <Logo />
+            <span className='text-xl font-semibold tracking-[-.01em]'>BlueAlpha</span>
           </a>
         </div>
         <Link href='/about'>About</Link>
