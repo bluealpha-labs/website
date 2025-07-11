471f50b0ec6e7945cf85d2174d27dacb078db379
samy <samy@samyvs.com>
2025-07-10 19:04:50 -0700

    Update content component

--- src/pages/_components/hero/content.tsx
+++ src/pages/_components/hero/content.tsx
@@ -7,11 +7,11 @@ import { Title } from './title'
 export function Content() {
   return (
     <motion.div
-      className='flex flex-1 flex-col justify-center gap-8'
+      className='flex flex-col gap-8'
       initial={{ opacity: 0, y: 40 }}
       animate={{ opacity: 1, y: 0 }}
       transition={smoothTransition}>
-      <div className='flex flex-col gap-6'>
+      <div className='flex flex-col gap-4 lg:gap-6'>
         <Title />
         <Description />
       </div>
