36b03431edd9a2624ade588dd1e35b311996a17f
samy <samy@samyvs.com>
2025-07-10 18:32:35 -0700

    Update circle component

--- src/components/animations/circle.tsx
+++ src/components/animations/circle.tsx
@@ -6,7 +6,10 @@ export type CircleProps = ComponentProps<'div'>
 export function Circle({ className, ...props }: CircleProps) {
   return (
     <div
-      className={cn('z-10 flex size-12 items-center justify-center rounded-full bg-gray-100', className)}
+      className={cn(
+        'z-10 flex size-12 items-center justify-center rounded-full bg-stone-100 text-stone-600',
+        className
+      )}
       {...props}
     />
   )
