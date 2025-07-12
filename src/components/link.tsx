6fca73f0bc071b0e7963dc7d11793511f501f2cc
samy <samy@samyvs.com>
2025-07-11 17:48:43 -0700

    Add footer

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -4,24 +4,31 @@ import type { ComponentProps } from 'react'
 import { tv, type VariantProps } from 'tailwind-variants'
 
 export const linkStyles = tv({
-  base: 'font-semibold tracking-[.01em] transition-colors ease-out select-none',
+  base: 'transition-colors ease-out select-none tracking-[.01em]',
   variants: {
     variant: {
-      default: 'hover:text-stone-800'
+      default: 'font-semibold',
+      simple: 'text-sm font-[450]'
+    },
+    color: {
+      default: 'hover:text-stone-800',
+      inverse: 'text-white hover:text-stone-100',
+      inverseMuted: 'text-stone-400 hover:text-stone-300'
     }
   },
   defaultVariants: {
-    variant: 'default'
+    variant: 'default',
+    color: 'default'
   }
 })
 
 export type LinkProps = ComponentProps<typeof motion.a> & VariantProps<typeof linkStyles>
 
-export function Link({ variant, className, ...props }: LinkProps) {
+export function Link({ variant, color, className, ...props }: LinkProps) {
   return (
     <motion.a
       layout
-      className={linkStyles({ variant, className })}
+      className={linkStyles({ variant, color, className })}
       transition={snappyTransition}
       {...props}
     />
