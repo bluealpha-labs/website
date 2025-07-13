d0dfa501592e622c84f82a2f95108fb5175e1b97
samy <samy@samyvs.com>
2025-07-13 16:05:13 -0700

    Add cal

---
+++ src/components/call-with-founder.tsx
@@ -0,0 +1,28 @@
+import { getCalApi } from '@calcom/embed-react'
+import { useEffect } from 'react'
+import { Button } from './button'
+
+export function CallWithFounder() {
+  useEffect(() => {
+    async function init() {
+      const cal = await getCalApi({ namespace: 'call-with-founder' })
+      cal('ui', {
+        theme: 'light',
+        cssVarsPerTheme: { light: { 'cal-brand': '#2563eb' }, dark: { 'cal-brand': '#2563eb' } },
+        hideEventTypeDetails: false,
+        layout: 'month_view'
+      })
+    }
+
+    init()
+  }, [])
+
+  return (
+    <Button
+      data-cal-namespace='call-with-founder'
+      data-cal-link='bluealpha/call-with-founder'
+      data-cal-config='{"layout":"month_view"}'>
+      Call with founder
+    </Button>
+  )
+}
