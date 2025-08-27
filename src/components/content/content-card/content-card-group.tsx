585a83bbc27ef2baf818f9342a59a53f20958064
samy <samy@samyvs.com>
2025-08-27 10:56:48 -0700

    Enhance ContentCardGroup component with gap and direction props
    - Updated ContentCardGroup in content-card-group.tsx to accept gap and direction props.
    - Integrated VariantProps from tailwind-variants for improved styling flexibility.
    - Added a new gap option (1) to the cva utility for Tailwind CSS styling.

--- src/components/content/content-card/content-card-group.tsx
+++ src/components/content/content-card/content-card-group.tsx
@@ -1,5 +1,6 @@
 import { cva } from '#utils/cva.ts'
 import type { ComponentProps } from 'react'
+import type { VariantProps } from 'tailwind-variants'
 
 const styles = cva({
   base: 'flex',
@@ -8,6 +9,7 @@ const styles = cva({
       column: 'flex-col'
     },
     gap: {
+      1: 'gap-1',
       2: 'gap-2',
       4: 'gap-4',
       6: 'gap-6'
@@ -15,12 +17,12 @@ const styles = cva({
   }
 })
 
-type Props = ComponentProps<'div'>
+type Props = ComponentProps<'div'> & VariantProps<typeof styles>
 
-export function ContentCardGroup({ className, ...props }: Props) {
+export function ContentCardGroup({ direction, gap, className, ...props }: Props) {
   return (
     <div
-      className={styles({ className })}
+      className={styles({ direction, gap, className })}
       {...props}
     />
   )
